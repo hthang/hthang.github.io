4d5d12187698044eb742ffc115066332eca7a35d
hthang <cwalk3422@gmail.com>
2021-07-18 11:35:16 +0700

    upload

--- mia/scripts/selected-footer.js
+++
@@ -1,8 +0,0 @@
-const a = document.querySelectorAll(".footer-select__name");
-a.forEach((selected) => {
-    const option = selected.previousElementSibling;
-    selected.addEventListener("click", function () {
-        option.classList.toggle("active--footer");
-    })
-
-})
