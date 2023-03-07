ac7cf63aa39408433c72ff74b919e600be2538f2
hthang <cwalk3422@gmail.com>
2023-03-08 00:04:31 +0700

    deploy

---
+++ testPhp/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+    <?php include 'header.html'; ?>
+</head>
+<body>
+    <?php include 'body.html'; ?>
+    <?php include 'footer.html'; ?>
+</body>
+</html>
